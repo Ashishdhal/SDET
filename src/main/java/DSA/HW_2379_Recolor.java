e6112ca092cd7d980583c2c40c80c1332b7c0c80
Ashishdhal <ashishdhal369@gmail.com>
2023-09-26 17:34:13 +0530

    DSA

--- src/main/java/DSA/HW_2379_Recolor.java
+++ src/main/java/DSA/HW_2379_Recolor.java
@@ -1,4 +1,8 @@
 package DSA;
+//1.Initialize 2 pointers left=0, right=0 with and balckcount =0 
+//2.Iterate till the kth index to get the first frame and find the max black count
+//3. continue the operation for rest of the string and slide the window while making left++, right++
+//4. subtract the max from k and return the value
 
 import org.junit.Test;
 
@@ -22,9 +26,14 @@ public void test1() {
 	Assert.assertEquals(0, minimumRecolors);
 }
 public int minimumRecolors(String blocks, int k) { //"WBBWWBBWBW"
-	  
+	
+	//1.Initialize 2 pointers left=0, right=0 with and balckcount =0 
+	//2.Iterate till the kth index to get the first frame and find the max black count
+	//3. continue the operation for rest of the string and slide the window while making left++, right++
+	//4. subtract the max from k and return the value
+ 
     int max = -1, left = 0,right=0, blackCount = 0;
-  
+  //2.Iterate till the kth index to get the first frame and find the max black count
     while(right < k) {
         if(blocks.charAt(right)=='B')
         	blackCount++;
@@ -33,7 +42,7 @@ public int minimumRecolors(String blocks, int k) { //"WBBWWBBWBW"
     //finding max
     max = Math.max(blackCount, max);
     
-    //continue the operation for rest of the string
+    //3. continue the operation for rest of the string and slide the window while making left++, right++
     while( right < blocks.length()){
     	//right++;
     	
@@ -48,7 +57,7 @@ public int minimumRecolors(String blocks, int k) { //"WBBWWBBWBW"
     	right++;
         max = Math.max(blackCount, max);
     }
-
+//4. subtract the max from k and return the value
    // return max;
     System.out.println("max :"+max);
 	
